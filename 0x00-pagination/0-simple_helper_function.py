06ea1fb031ab53e5004a9929dcb687d50463c97d
denniskofismith <denniskofismith@gmail.com>
2024-11-28 13:11:56 +0000

    Pagnation first exercise

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-from typing import Tuple
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    
+def index_range(page: int, page_size: int) -> tuple:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
